ddf58698781b8ad678811db1cf33039e5f3c6b5d
Akhil <akhilakhil6107@gmail.com>
2026-06-07 17:26:00 +0530

    Fix AnalyticsResponse Redis serialization

--- backend/src/main/java/com/paymentplatform/dto/response/AnalyticsResponse.java
+++ backend/src/main/java/com/paymentplatform/dto/response/AnalyticsResponse.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -16,28 +17,39 @@ import java.util.Map;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class AnalyticsResponse {
+public class AnalyticsResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private Long totalPayments;
     private Long successfulPayments;
     private Long failedPayments;
     private Long pendingPayments;
     private Long retriedPayments;
+
     private BigDecimal totalVolume;
     private BigDecimal successVolume;
+
     private Double successRate;
     private Double averagePaymentAmount;
+
     private List<DailyStats> dailyStats;
+
     private Map<String, Long> paymentMethodBreakdown;
     private Map<String, Long> statusBreakdown;
+
     private List<RecentActivity> recentActivity;
+
     private ZonedDateTime generatedAt;
 
     @Data
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class DailyStats {
+    public static class DailyStats implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
         private String date;
         private Long total;
         private Long success;
@@ -49,7 +61,10 @@ public class AnalyticsResponse {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class RecentActivity {
+    public static class RecentActivity implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
         private String paymentReference;
         private BigDecimal amount;
         private String currency;
